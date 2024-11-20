115e61a3eb7f50cdcf1b5deedf7cc66e93d3a57e
Andrei <witeline@mail.ru>
2024-11-20 17:56:26 +0300

    Добавление всех форм приложений

--- Delphi_project/MyDb/Unit1.pas
+++ Delphi_project/MyDb/Unit1.pas
@@ -4,12 +4,29 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls;
+  Dialogs, StdCtrls, Menus;
 
 type
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    MainMenu1: TMainMenu;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    N5: TMenuItem;
+    N6: TMenuItem;
+    N7: TMenuItem;
+    N8: TMenuItem;
+    N9: TMenuItem;
+    N10: TMenuItem;
+    N11: TMenuItem;
+    Button3: TButton;
+    Button4: TButton;
     procedure Button1Click(Sender: TObject);
+    procedure N6Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -28,6 +45,16 @@ uses Unit2;
 procedure TForm1.Button1Click(Sender: TObject);
 begin
 form2.Show();
+end;
+
+procedure TForm1.N6Click(Sender: TObject);
+begin
+form2.Show();
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+form3.Show()
 end;
 
 end.
