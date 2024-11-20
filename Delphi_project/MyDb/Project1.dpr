9f1eef32340de05abe06698f01c9c81850108a4a
Andrei <witeline@mail.ru>
2024-11-20 18:45:28 +0300

    Добавление форм

--- Delphi_project/MyDb/Project1.dpr
+++ Delphi_project/MyDb/Project1.dpr
@@ -2,16 +2,20 @@ program Project1;
 
 uses
   Forms,
-  Unit1 in 'Unit1.pas' {Form1},
+  Unit1 in 'Unit1.pas' {GlavnoeMeny},
   Unit2 in 'Unit2.pas' {Form2},
-  prichod in 'prichod.pas' {Form3};
+  prichod in 'prichod.pas' {Form3},
+  Unit4 in 'H:\MyDb\Unit4.pas' {PostavForm},
+  Unit5 in 'H:\MyDb\Unit5.pas' {PrichodForm};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TGlavnoeMeny, GlavnoeMeny);
   Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TForm3, Form3);
+  Application.CreateForm(TPostavForm, PostavForm);
+  Application.CreateForm(TPrichodForm, PrichodForm);
   Application.Run;
 end.
