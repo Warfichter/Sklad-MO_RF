2cf04e74b7cb4f16e238452e8f77eea5920cd97e
Andrei <witeline@mail.ru>
2024-11-21 20:40:22 +0300

    ДОБАВЛЕН (fast Report)

--- Delphi_project/MyDb/Unit7.pas
+++ Delphi_project/MyDb/Unit7.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, DB, mySQLDbTables, Grids, DBGrids, StdCtrls;
+  Dialogs, DB, mySQLDbTables, Grids, DBGrids, StdCtrls, frxClass, frxDBSet;
 
 type
   TForm7 = class(TForm)
@@ -14,7 +14,11 @@ type
     DBGrid1: TDBGrid;
     MySQLDatabase1: TMySQLDatabase;
     edit1: TEdit;
+    Button2: TButton;
+    frxDBDataset1: TfrxDBDataset;
+    frxReport1: TfrxReport;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -31,13 +35,17 @@ implementation
 procedure TForm7.Button1Click(Sender: TObject);
 begin
   if (MySQLQuery1.Active = false)
-   then
-   begin
-  MySQLQuery1.ParamByName('par1').AsString:= edit1.Text+'%';
-  MySQLQuery1.Active:= true
-   end
-  else
-  MySQLQuery1.Active:= false;
+  then
+  begin
+    MySQLQuery1.ParamByName('par1').AsString := edit1.Text + '%';
+    MySQLQuery1.Active := true;
+    end
+   else
+    MySQLQuery1.Active := false;
+  end;
+procedure TForm7.Button2Click(Sender: TObject);
+begin
+frxReport1.ShowReport();
 end;
 
 end.
